f8321f1e7b3f7e43c495d4adf38a926ac3cd4a91
CollieHandymanServices <colliezach27@gmail.com>
2026-08-17 21:18:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+if (menuToggle && nav) {
+  menuToggle.addEventListener("click", () => nav.classList.toggle("open"));
+  document.querySelectorAll(".nav a").forEach(a => a.addEventListener("click", () => nav.classList.remove("open")));
+}
