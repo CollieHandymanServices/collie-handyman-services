380b9b5454e771066caac6b5b7d10046f9e64797
CollieHandymanServices <colliezach27@gmail.com>
2026-08-17 21:24:33 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-
-const menuToggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector(".nav");
-if (menuToggle && nav) {
-  menuToggle.addEventListener("click", () => nav.classList.toggle("open"));
-  document.querySelectorAll(".nav a").forEach(a => a.addEventListener("click", () => nav.classList.remove("open")));
-}
